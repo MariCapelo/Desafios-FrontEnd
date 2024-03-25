ea73c86b964d79a3e13ad981caa70bed2e9ae1e4
MariCapelo <marina.gac30@gmail.com>
2024-03-25 15:08:36 -0300

    troca de botao e overflow inseridos

--- faq-accordion-main/code.js
+++ faq-accordion-main/code.js
@@ -18,53 +18,66 @@ function show1()
     if(aparecer1 === 0)
     {
         frase1.style.display = "block";
+        BotaoPlus1.src = "assets/images/icon-minus.svg";
         aparecer1 = 1;
     }
     else
     {
         frase1.style.display = "none";
+        BotaoPlus1.src = "assets/images/icon-plus.svg"
         aparecer1 = 0;
     }
+   
 }
 function show2()
 {
     if(aparecer2 === 0)
     {
         frase2.style.display = "block";
+        BotaoPlus2.src = "assets/images/icon-minus.svg";
         aparecer2 = 1;
     }
     else
     {
         frase2.style.display = "none";
+        BotaoPlus2.src = "assets/images/icon-plus.svg"
         aparecer2 = 0;
     }
+    
 }
 function show3()
 {
     if(aparecer3 === 0)
     {
         frase3.style.display = "block";
+        BotaoPlus3.src = "assets/images/icon-minus.svg";
         aparecer3 = 1;
     }
     else
     {
         frase3.style.display = "none";
+        BotaoPlus3.src = "assets/images/icon-plus.svg"
         aparecer3 = 0;
     }
+    
 }
 function show4()
 {
     if(aparecer4 === 0)
     {
         frase4.style.display = "block";
+        BotaoPlus4.src = "assets/images/icon-minus.svg";
         aparecer4 = 1;
     }
     else
     {
         frase4.style.display = "none";
+        BotaoPlus4.src = "assets/images/icon-plus.svg"
         aparecer4 = 0;
     }
+    
 }
+
 
 BotaoPlus1.addEventListener('click', show1);
 BotaoPlus2.addEventListener('click', show2);
