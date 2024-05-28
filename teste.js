da35768b831d581676d06320a4d670b5a66cb444
MariCapelo <marina.gac30@gmail.com>
2024-05-28 12:14:26 -0300

    adicionando mais desafios

---
+++ teste.js
@@ -0,0 +1,16 @@
+// const nome = 'Marina';
+// const sobreNome = 'Capelo';
+
+// // concatenação normal
+// const NomeCompleto1 = nome + ' ' + sobreNome;
+// console.log(NomeCompleto1);
+
+// //template strings 
+// const NomeCompleto2 = `${nome} ${sobreNome}`;
+// console.log(NomeCompleto2);
+
+const fruta = 'maçã';
+const num = 5;
+
+const num_fruta = `${num} ${fruta}s`
+console.log(num_fruta);
